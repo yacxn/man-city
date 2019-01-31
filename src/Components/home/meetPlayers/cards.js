aa3d17c49881724af85491246a0741d7c37e7cb7
yacine <yacinebenz3@gmail.com>
2019-01-31 23:48:17 +0000

    finished card player

--- src/Components/home/meetPlayers/cards.js
+++ src/Components/home/meetPlayers/cards.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {easePolyOut} from 'd3-ease';
 import Animate from 'react-move/Animate';
 import Otamendi from '../../../Resources/images/players/Otamendi.png';
+import PlayerCard from '../../ui/playerCard';
 
 export default class HomeCards extends Component {
                  state = {
@@ -49,7 +50,12 @@ export default class HomeCards extends Component {
                                 bottom
                             }}
                          >
-                         hdsbdjb
+                         <PlayerCard
+                          number="30"
+                          name="nicolas"
+                          lastname="Otamendi"
+                          bck={Otamendi}
+                         />
                          </div>)
                        }}
                      </Animate>
